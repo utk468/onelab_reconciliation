a7294d454fd40506869dee58b5918bfd3fa1cff4
utk468 <utkarshpise468@gmail.com>
2026-04-29 23:57:55 +0530

    added html

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.post("/generate-data")
 async def generate_data():
